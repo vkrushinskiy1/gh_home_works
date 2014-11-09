2d0f4591538fd42a314d790543946b384d99eee4
Admin <admin@Mac-Admin.local>
2014-11-09 12:07:20 +0300

    Car2 was renamed

--- VadimKrushinskiy/AppDelegate.m
+++ VadimKrushinskiy/AppDelegate.m
@@ -6,40 +6,33 @@
 //  Copyright (c) 2014 Admin. All rights reserved.
 //
 
+#import "GHCar.h"
+
 #import "AppDelegate.h"
-
-@interface AppDelegate ()
-
-@end
 
 @implementation AppDelegate
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    
+    GHCar *car1 = [[GHCar alloc] initWithEngineCapacity:2.5 andHorsePower:192];
+    car1.topSpeed = 230.0;
+    car1.mark = @"BMW";
+    [car1 printCarIformation];
+    
+    GHCar *AnotherCar = [[GHCar alloc] initWithEngineCapacity:2.4 andHorsePower:180];
+    AnotherCar.topSpeed = 225.0;
+    AnotherCar.mark = @"Audi";
+    [AnotherCar printCarIformation];
+    
+    if ([car1 isFasterThanCar:AnotherCar]) {
+        NSLog(@"%@ is faster. speed - %0.0f", car1.mark, car1.topSpeed);
+    } else {
+        NSLog(@"%@ is faster. speed - %0.0f", AnotherCar.mark, AnotherCar.topSpeed);
+    }
+    
+    
     return YES;
-}
-
-- (void)applicationWillResignActive:(UIApplication *)application {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-}
-
-- (void)applicationDidEnterBackground:(UIApplication *)application {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-}
-
-- (void)applicationWillEnterForeground:(UIApplication *)application {
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-}
-
-- (void)applicationDidBecomeActive:(UIApplication *)application {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-}
-
-- (void)applicationWillTerminate:(UIApplication *)application {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
 @end
